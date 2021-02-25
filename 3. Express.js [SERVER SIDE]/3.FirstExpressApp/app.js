4b29f1c93a170e33ef7bfead740269f03c35ee41
George <angelov2000@abv.bg>
2021-02-25 10:33:05 +0200

    added new materials

---
+++ 3. Express.js [SERVER SIDE]/3.FirstExpressApp/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('<a href="/contact">Contact Us</a> <br> <a href="/about">About Us</a>');
+});
+
+app.get('/contact', (req, res)=>{
+    res.send('<h1>Contact Us Page</h1>');
+})
+
+app.get('/about', (req, res)=> {
+    res.send('<h1>About Us Page</h1>');
+})
+
+app.get('*', (req, res)=> {
+    res.send('404. This page does not exist. <a href="/">Go to HomePage</a>')
+})
+
+
+app.listen(3000, ()=> {
+    console.log('Server started on port 3000');
+});
---
+++ 3. Express.js [SERVER SIDE]/5.Routes Parameters/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/questions/:id/:title/', (req, res)=> {
+    res.send('This route handles all the questions.')
+});
+
+app.get('/users/:id/:username', (req, res)=> {
+    let userId = req.params.id;
+    let user = req.params.username;
+
+    res.send('This route will handle '+user+' profile that has ID: '+userId);
+})
+
+
+app.listen(3000, ()=> {
+    console.log('Server started on port 3000.');
+})
