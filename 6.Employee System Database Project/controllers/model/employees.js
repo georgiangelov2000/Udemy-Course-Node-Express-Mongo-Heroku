fac71597ee07f63fb14009031e6a0419a81cd247
George <angelov2000@abv.bg>
2021-02-26 19:10:46 +0200

    Deleted

--- 6.Employee System Database Project/controllers/model/employees.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let employeeSchema = new mongoose.Schema({
-    name : String,
-    designation : String,
-    salary : Number
-});
-
-module.exports=mongoose.model('Employee',employeeSchema);
