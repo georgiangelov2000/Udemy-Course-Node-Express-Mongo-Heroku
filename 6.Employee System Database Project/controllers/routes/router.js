eedb906815a6f3e9ac209326720e31a5f7613c5e
George <angelov2000@abv.bg>
2021-02-26 19:11:19 +0200

    Add resources for employees

---
+++ 6.Employee System Database Project/controllers/routes/router.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router();
+const Employee = require("../model/employees");
+
+//Rendering Employee
+router.get("/", (req, res) => {
+  Employee.find({})
+    .then((employees) => {
+      res.render("index", { employees: employees });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Router to Add Employee
+router.get("/employee/add-employee", (req, res) => {
+  res.render("add-employee");
+});
+
+//Router to Search Form
+router.get("/employee/search", (req, res) => {
+  res.render("search",{ employee: "" });
+});
+
+
+//Get current Employee
+router.get("/employee", (req, res) => {
+  const searchQuery = { name: req.query.name };
+
+  Employee.findOne(searchQuery)
+    .then((employee) => {
+      res.render("search", { employee: employee });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Router to Update Form
+router.get("/update/:id", (req, res) => {
+  const searchQuery = { _id: req.params.id };
+  Employee.findOne(searchQuery)
+    .then((employee) => {
+      res.render("update-employee", { employee: employee });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Post Employee
+router.post("/employee/add-employee", (req, res) => {
+  const newEmployee = {
+    name: req.body.name,
+    designation: req.body.designation,
+    salary: req.body.salary,
+  };
+  Employee.create(newEmployee)
+    .then((employee) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Post Update Employee
+
+router.put("/update/:id", (req, res) => {
+  const searchQuery = { _id: req.params.id };
+
+  Employee.updateOne(searchQuery, {
+    $set: {
+      name: req.body.name,
+      designation: req.body.designation,
+      salary: req.body.salary,
+    },
+  })
+    .then((employee) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
---
+++ 6.Employee System Database Project/server.js
@@ -0,0 +1,39 @@
+//modules
+const express=require('express');
+const app=express();
+const path=require('path');
+const dotEnv=require('dotenv');
+const mongoose=require('mongoose');
+const methodOverride = require('method-override');
+const bodyParser=require('body-parser');
+
+
+//bodyParser
+app.use(bodyParser.urlencoded({extended:true}))
+
+//Overrride Method
+app.use(methodOverride('_method'));
+
+//connecting to Enviroment Variavles
+dotEnv.config({path:'./config.env'});
+
+//connecting to MONGODB database
+mongoose.connect(process.env.DATABASE_LOCAL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+});
+
+//UI adjustment
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+//Router
+const router=require('./controllers/routes/router')
+app.use(router);
+
+
+const port=process.env.PORT;
+app.listen(port,()=>{
+    console.log(`Server listening on port ${port}`)
+})
