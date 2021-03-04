dbbd80e5a9ba012a91a19e5f4e7a508d2b6ff05c
George <angelov2000@abv.bg>
2021-03-04 22:05:25 +0200

    Added dashboard-router

--- 10.E-Commerce Website Database/functionality/routes/dashboard-router.js
+++ 10.E-Commerce Website Database/functionality/routes/dashboard-router.js
@@ -5,7 +5,7 @@ const Product=require('../models/productmodel');
 
 //Get Routes
 router.get('/product/new',(req,res)=>{
-    res.render('newproduct')
+    res.render('admin-dashboard/newproduct')
 })
 router.get('/product/update',(req,res)=>{
     res.render('admin-dashboard/update')
