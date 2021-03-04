884cfb3d3ff8e187f0a0d7a509348ab6da969a93
George <angelov2000@abv.bg>
2021-03-04 10:30:22 +0200

    added register and login logic

--- 10.E-Commerce Website Database/functionality/routes/router.js
+++ 10.E-Commerce Website Database/functionality/routes/router.js
@@ -2,6 +2,16 @@
 const express=require('express');
 const router=express.Router();
 const passport=require('passport');
+const User=require('../models/usermodel')
+
+// Checks if user is authenticated
+function isAuthenticatedUser(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    req.flash('error_msg', 'Please Login first to access this page.')
+    res.redirect('/login');
+}
 
 //Get Routes
 router.get('/',(req,res)=>{
@@ -12,11 +22,50 @@ router.get('/login',(req,res)=>{
     res.render('login')
 })
 
+router.get('/dashboard',isAuthenticatedUser,(req,res)=>{
+    res.render('dashboard')
+})
+
+router.get('/signup',(req,res)=>{
+    res.render('signup')
+})
+
+router.get('/logout',isAuthenticatedUser,(req,res)=>{
+    req.logOut();
+    req.flash('success_msg', 'You have been logged out.');
+    res.redirect('/login');
+})
+
 //Post Routes
+
+//login
 router.post('/login', passport.authenticate('local', {
     successRedirect : '/dashboard',
     failureRedirect : '/login',
     failureFlash: 'Invalid email or password. Try Again!!!'
 }));
+
+//register
+router.post('/signup',(req,res)=>{
+    let {name,email,password}=req.body;
+    let userData={
+        name:name,
+        email:email
+    };
+    User.register(userData,password,(error,data)=>{
+        if(error){
+            req.flash('error_msg', 'ERROR: '+error);
+            res.redirect('/signup');
+        }
+        else if(userData.name.length<4 || password.length<4){
+            req.flash('error_msg', 'Name or Password is too short. Please try again!');
+            res.redirect('/signup');
+        }
+        req.flash('success_msg', 'Account created successfully');
+        res.redirect('/signup');
+    });
+});
+
+//loguot
 
 module.exports = router;
--- 10.E-Commerce Website Database/server.js
+++ 10.E-Commerce Website Database/server.js
@@ -3,12 +3,58 @@ const express=require('express');
 const app=express();
 const dotenv=require('dotenv');
 const path=require('path')
+const passport=require('passport');
+const mongoose = require('mongoose');
+const session=require('express-session')
+const flash=require('connect-flash');
+const bodyParser=require('body-parser');
+const localStrategy=require('passport-local').Strategy;
 
 //Router
 const router=require('./functionality/routes/router');
 
+//User
+const User=require('./functionality/models/usermodel')
+
 // Setting up dotenv
 dotenv.config({path:'./config.env'})
+
+//Setting up DB
+mongoose.connect(process.env.DB_LOCAL,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useCreateIndex:true,
+    useUnifiedTopology: true
+});
+
+//Setting up session
+app.use(session({
+    secret : 'Just a simple login/sign up application.',
+    resave : true,
+    saveUninitialized : true
+}));
+
+//Setting up passport
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy({usernameField:'email'},User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Middleware flash messages
+app.use(flash());
+
+//setting middlware globally
+app.use((req, res, next)=> {
+    res.locals.success_msg = req.flash(('success_msg'));
+    res.locals.error_msg = req.flash(('error_msg'));
+    res.locals.error = req.flash(('error'));
+    res.locals.currentUser = req.user;
+    next();
+});
+
+//Setting up bodyparser
+app.use(bodyParser.urlencoded({extended:true}));
 
 //Path
 app.set('views',path.join(__dirname,'views'));
@@ -18,8 +64,6 @@ app.set('view engine','ejs')
 
 //Middleware router
 app.use(router)
-
-
 
 app.listen(process.env.PORT,()=>{
     console.log('Server is started');
