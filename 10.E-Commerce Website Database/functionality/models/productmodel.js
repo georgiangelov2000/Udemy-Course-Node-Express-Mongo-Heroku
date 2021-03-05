1b68ac56a05c9667975d9e6ba482290923750f62
George <angelov2000@abv.bg>
2021-03-05 09:34:07 +0200

    Added instock template

--- 10.E-Commerce Website Database/functionality/models/productmodel.js
+++ 10.E-Commerce Website Database/functionality/models/productmodel.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 
-let productsScheme=new mongoose.Schema({
+let productScheme=new mongoose.Schema({
     imageUrl:String,
     name:String,
     description:String,
     price:Number
 })
-// module.exports = mongoose.model('Product', productScheme);
+ module.exports = mongoose.model('Product', productScheme);
--- 10.E-Commerce Website Database/functionality/routes/dashboard-router.js
+++ 10.E-Commerce Website Database/functionality/routes/dashboard-router.js
@@ -10,6 +10,28 @@ router.get('/product/new',(req,res)=>{
 router.get('/product/update',(req,res)=>{
     res.render('admin-dashboard/update')
 })
+router.get('/product/instock',(req,res)=>{
+    res.render('admin-dashboard/instock')
+})
+
+//Post Routes
+router.post('/product/new',(req,res)=>{
+    const newProduct={
+        image:req.body.imageUrl,
+        name: req.body.name,
+        description:req.body.description,
+        price:req.body.price
+    };
+    Product.create(newProduct)
+    .then((product)=>{
+        req.flash('success_msg','Product data added to database successfully.')
+        res.redirect('/dashboard')
+    })
+    .catch((error)=>{
+        req.flash('error_msg','Error:'+error)
+        res.redirect('/newproduct')
+    })
+})
 
 module.exports = router
 
