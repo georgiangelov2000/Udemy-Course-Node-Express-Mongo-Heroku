7b1f26c128eb8439f57a12304c55031e4cceaced
George <angelov2000@abv.bg>
2021-03-06 12:55:55 +0200

    updated files

--- 10.E-Commerce Website Database/functionality/routes/dashboard-router.js
+++ 10.E-Commerce Website Database/functionality/routes/dashboard-router.js
@@ -1,137 +1,109 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-const multer=require('multer')
-let upload=multer({dest:'upload'})
-
-const Product=require('../models/productmodel');
+const Product = require("../models/productmodel");
 
 // Checks if user is authenticated
 function isAuthenticatedUser(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    req.flash('error_msg', 'Please Login first to access this page.')
-    res.redirect('/login');
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash("error_msg", "Please Login first to access this page.");
+  res.redirect("/login");
 }
 
 //Get Routes
 
 //Rendering Products
-router.get("/product/instock",isAuthenticatedUser,(req, res) => {
-    Product.find({})
-      .then((products) => {
-        res.render("admin-dashboard/instock", { products: products });
-      })
-      .catch((err) => {
-        req.flash('error_msg','ERROR:' +err)
-        res.redirect('/dashboard');
-      });
-  });
+router.get("/product/instock", isAuthenticatedUser, (req, res) => {
+  Product.find({})
+    .then((products) => {
+      res.render("admin-dashboard/instock", { products: products });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "ERROR:" + err);
+      res.redirect("/dashboard");
+    });
+});
 
 //Router to Update Form
-router.get("/product/update/:id",isAuthenticatedUser, (req, res) => {
-    const searchQuery = { _id: req.params.id };
-    Product.findOne(searchQuery)
-      .then((product) => {
-        res.render("admin-dashboard/update", { product: product });
-      })
-      .catch((error) => {
-      req.flash('error_msg','ERROR:' +error)
-      res.redirect('/dashboard');
-      });
-  });
+router.get("/product/update/:id", isAuthenticatedUser, (req, res) => {
+  const searchQuery = { _id: req.params.id };
+  Product.findOne(searchQuery)
+    .then((product) => {
+      res.render("admin-dashboard/update", { product: product });
+    })
+    .catch((error) => {
+      req.flash("error_msg", "ERROR:" + error);
+      res.redirect("/dashboard");
+    });
+});
 
-router.get('/product/new',isAuthenticatedUser,(req,res)=>{
-    res.render('admin-dashboard/newproduct')
-})
-router.get('/product/update',isAuthenticatedUser,(req,res)=>{
-    res.render('admin-dashboard/update')
-})
-router.get('/product/instock',isAuthenticatedUser,(req,res)=>{
-    res.render('admin-dashboard/instock')
-})
-router.get('/products/myproducts',(req,res)=>{
-  res.render('admin-dashboard/admin-products/myproducts')
-})
+router.get("/product/new", isAuthenticatedUser, (req, res) => {
+  res.render("admin-dashboard/newproduct");
+});
+router.get("/product/update", isAuthenticatedUser, (req, res) => {
+  res.render("admin-dashboard/update");
+});
+router.get("/product/instock", isAuthenticatedUser, (req, res) => {
+  res.render("admin-dashboard/instock");
+});
+router.get("/products/myproducts", (req, res) => {
+  res.render("admin-dashboard/admin-products/myproducts");
+});
 
 //Post Routes
 
-router.post('/product/new',(req,res)=>{
-  const newProduct={
-      imageUrl: req.body.imageUrl,
-      name: req.body.name,
-      description: req.body.description,
-      price: req.body.price
+router.post("/product/new",(req, res) => {
+  const newProduct = {
+    imageUrl: req.body.imageUrl,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
   };
   Product.create(newProduct)
-  .then((product)=>{
-      req.flash('success_msg','Product data added to database successfully.')
-      res.redirect('/dashboard')
-  })
-  .catch((error)=>{
-      req.flash('error_msg','Error:'+error)
-      res.redirect('/product/new')
-  })
-})
+    .then((product) => {
+      req.flash("success_msg", "Product data added to database successfully.");
+      res.redirect("/dashboard");
+    })
+    .catch((error) => {
+      req.flash("error_msg", "Error:" + error);
+      res.redirect("/product/new");
+    });
+});
 
 router.put("/product/update/:id", (req, res) => {
-    const searchQuery = { _id: req.params.id };
-  
-    Product.updateOne(searchQuery, {
-      $set: {     
-        imageUrl: req.body.imageUrl,
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price
-    },
-    })
-      .then((product) => {
-        req.flash('success_msg','Product data updated successfully')
-        res.redirect("/dashboard");
-      })
-      .catch((error) => {
-        req.flash('error_msg','ERROR:' +error)
-        res.redirect('/dashboard');
-      });
-  });
+  const searchQuery = { _id: req.params.id };
 
-  router.delete('/product/delete/:id',(req,res)=>{
-    const id= {_id:req.params.id}
-    Product.findByIdAndDelete(id)
-    .then(product=>{
-      req.flash('success_msg','Product deleted succesfully.')
-      res.redirect('/dashboard');
-    })
-    .catch(error=>{
-      req.flash('error_msg','ERROR:' +error)
-      res.redirect('/dashboard');
-    })
-  })
-
-
-module.exports = router
-
-
-
-
-/*
-router.post('/product/new',upload.single('singleImage'),(req,res,next)=>{
-  
-  const newProduct={
+  Product.updateOne(searchQuery, {
+    $set: {
       imageUrl: req.body.imageUrl,
       name: req.body.name,
       description: req.body.description,
-      price: req.body.price
-  };
-  Product.create(newProduct)
-  .then((product)=>{
-      req.flash('success_msg','Product data added to database successfully.')
-      res.redirect('/dashboard')
+      price: req.body.price,
+    },
   })
-  .catch((error)=>{
-      req.flash('error_msg','Error:'+error)
-      res.redirect('/product/new')
-  })
-})
-*/
+    .then((product) => {
+      req.flash("success_msg", "Product data updated successfully");
+      res.redirect("/dashboard");
+    })
+    .catch((error) => {
+      req.flash("error_msg", "ERROR:" + error);
+      res.redirect("/dashboard");
+    });
+});
+
+router.delete("/product/delete/:id", (req, res) => {
+  const id = { _id: req.params.id };
+  Product.findByIdAndDelete(id)
+    .then((product) => {
+      req.flash("success_msg", "Product deleted succesfully.");
+      res.redirect("/dashboard");
+    })
+    .catch((error) => {
+      req.flash("error_msg", "ERROR:" + error);
+      res.redirect("/dashboard");
+    });
+});
+
+module.exports = router;
+
